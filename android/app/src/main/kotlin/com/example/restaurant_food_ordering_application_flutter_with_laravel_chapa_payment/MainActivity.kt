478df71793391247cbb7d7a310c55e8a22b2251a
Medhin12 <medhin@360ground.com>
2024-01-29 01:07:30 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/restaurant_food_ordering_application_flutter_with_laravel_chapa_payment/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.restaurant_food_ordering_application_flutter_with_laravel_chapa_payment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
